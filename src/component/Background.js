447ab17ded5c84db3447e3bd67f61b8e630f3b2c
Aayan saini <110893943+Aayansaini@users.noreply.github.com>
2022-12-22 19:35:08 +0530

    Shopy Working Flow...!

--- src/component/Background.js
+++ src/component/Background.js
@@ -3,6 +3,7 @@ function Background(){
         <div className="helo">
             <p>Online <span>Shopping</span> With <span>Shopy</span></p>
             <input id="search" placeholder="Search items" type={"search"}/>
+            <button><h4>Search-</h4></button>
         </div>
     )
 }
